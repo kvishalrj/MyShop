4758f42bf7e711e661806e4ee76602a78f19ec71
Vishal Kumar <kvishalrj2020@gmail.com>
2023-10-02 23:45:23 +0530

    Add price

--- shop/views.py
+++ shop/views.py
@@ -32,6 +32,7 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    thank = False
     if request.method=="POST":
         name = request.POST.get('name'," ")
         email = request.POST.get('email'," ")
@@ -39,7 +40,8 @@ def contact(request):
         desc = request.POST.get('desc'," ")
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-    return render(request, 'shop/contact.html')
+        thank = True
+    return render(request, 'shop/contact.html', {'thank':thank})
 
 def tracker(request):
     if request.method=="POST":
