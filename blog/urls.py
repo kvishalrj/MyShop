d47f9ac68f54d55940288e4e573c373ec722e457
Vishal Kumar <kvishalrj2020@gmail.com>
2023-10-03 01:42:57 +0530

    Blog page started

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="BlogHome"),
+    path("blogpost/", views.blogpost, name="blogPost"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def blogpost(request):
+    return render(request, 'blog/blogpost.html')
