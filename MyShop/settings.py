ab3815abea8c2174da59f874a332881f9f7397a9
Vishal Kumar <kvishalrj2020@gmail.com>
2023-11-24 19:56:10 +0530

    vercel host

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j4dc(##7bx&1vxf9#m&)hsjdnyq(%#u46$+v31oc5tx@*dnn!s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -132,5 +132,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
