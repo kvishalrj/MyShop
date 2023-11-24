0fb1e2fcd9c7d5212a06a4150d84003b524f2ae4
Vishal Kumar <kvishalrj2020@gmail.com>
2023-11-24 19:23:11 +0530

    database updated

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'MyShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgre1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
