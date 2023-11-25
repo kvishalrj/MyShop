99fe20ddc176dacb2a72edd4d908a80f6ff2864a
Vishal Kumar <kvishalrj2020@gmail.com>
2023-11-25 08:10:30 +0530

    updated python version

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo " BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
