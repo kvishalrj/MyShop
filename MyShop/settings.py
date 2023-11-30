16a80866800b0a47c9984101db790f2c59530909
Vishal Kumar <kvishalrj2020@gmail.com>
2023-11-30 21:53:45 +0530

    new update

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
